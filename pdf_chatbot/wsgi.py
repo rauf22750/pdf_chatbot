8f95459615424974c3ba5d7587ffc73666147bc5
Abdul Rauf <rauf22750@gmail.com>
2025-01-21 15:08:06 +0500

    first commit

--- pdf_chatbot/wsgi.py
+++ pdf_chatbot/wsgi.py
@@ -1,13 +1,14 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
+from django.core.management import execute_from_command_line
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pdf_chatbot.settings')
 
+# Collect static files
+execute_from_command_line(['manage.py', 'collectstatic', '--noinput'])
+
 application = get_wsgi_application()
-application = WhiteNoise(application)
-
-
+app = application
 
 
 
