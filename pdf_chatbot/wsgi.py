fe0813bf15db50c1528fbd7313e4c3981c7b93ec
Abdul Rauf <rauf22750@gmail.com>
2025-01-21 13:49:53 +0500

    first commit

--- pdf_chatbot/wsgi.py
+++ pdf_chatbot/wsgi.py
@@ -8,12 +8,21 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Add the project directory to the Python path
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pdf_chatbot.settings')
 
+from django.core.wsgi import get_wsgi_application
+
+# Get the WSGI application
 application = get_wsgi_application()
 
-# Add this for Vercel
+# Vercel needs the variable to be named 'app'
 app = application
+
+
