74cf3de6d2e9bbfc61f04b3b3d4fa506c589741f
Abdul Rauf <rauf22750@gmail.com>
2025-01-27 20:07:02 +0500

    first commit

--- build_files.sh
+++ build_files.sh
@@ -3,5 +3,3 @@ python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
 python3 manage.py runserver 
-python3 manage.py makemigrations
-python3 manage.py migrate
