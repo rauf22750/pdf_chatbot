c90ab42f085c24b4d2eef4adb892ab5cade05b95
Abdul Rauf <rauf22750@gmail.com>
2025-01-28 19:08:08 +0500

    first commit

--- build_files.sh
+++ build_files.sh
@@ -2,32 +2,31 @@
 set -e  # Exit on any error
 
 echo "Ensuring pip is installed..."
-python -m ensurepip --upgrade  # Use python instead of python3.9
+python3.9 -m ensurepip --upgrade
 if [ $? -ne 0 ]; then
   echo "Failed to install pip!"
   exit 1
 fi
 
 echo "Upgrading pip..."
-python -m pip install --upgrade pip
+python3.9 -m pip install --upgrade pip
 if [ $? -ne 0 ]; then
   echo "Failed to upgrade pip!"
   exit 1
 fi
 
 echo "Installing dependencies..."
-python -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 if [ $? -ne 0 ]; then
   echo "Failed to install dependencies!"
   exit 1
 fi
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput
 if [ $? -ne 0 ]; then
   echo "Failed to collect static files!"
   exit 1
 fi
 
 echo "Build completed."
-
