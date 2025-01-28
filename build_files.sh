43e458fc3c668a01b79e5bc78b2d83bdc3790494
Abdul Rauf <rauf22750@gmail.com>
2025-01-28 15:44:13 +0500

    first commit

--- build_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#!/bin/bash
-echo "Installing dependencies..."
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
-echo "Collecting static files..."
-python3 manage.py collectstatic --noinput --clear
-echo "Build completed."
