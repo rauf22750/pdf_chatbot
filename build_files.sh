93f28b64cab03c83b6633a5ec22b328ce6b17034
Abdul Rauf <rauf22750@gmail.com>
2025-01-28 21:15:36 +0500

    first commit

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 #!/bin/bash
-echo "Installing dependencies..."
-python3.9 -m pip install --upgrade pip
+# echo "Installing dependencies..."
+# python3.9 -m pip install --upgrade pip
+# python3.9 -m pip install -r requirements.txt
+# echo "Collecting static files..."
+# python3.9 manage.py collectstatic --noinput
+# echo "Build completed."
+#!/bin/bash
+# Build the project
+echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
-echo "Collecting static files..."
-python3.9 manage.py collectstatic --noinput
-echo "Build completed."
+
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
