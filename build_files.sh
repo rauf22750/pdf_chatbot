06ac27981420274c1298ff57c6afbd3fbf63c42c
Abdul Rauf <rauf22750@gmail.com>
2025-01-28 19:16:07 +0500

    first commit

--- build_files.sh
+++ build_files.sh
@@ -2,28 +2,28 @@
 set -e  # Exit on any error
 
 echo "Ensuring pip is installed..."
-Python -m ensurepip --upgrade
+python3 -m ensurepip --upgrade
 if [ $? -ne 0 ]; then
   echo "Failed to install pip!"
   exit 1
 fi
 
 echo "Upgrading pip..."
-Python -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 if [ $? -ne 0 ]; then
   echo "Failed to upgrade pip!"
   exit 1
 fi
 
 echo "Installing dependencies..."
-Python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 if [ $? -ne 0 ]; then
   echo "Failed to install dependencies!"
   exit 1
 fi
 
 echo "Collecting static files..."
-Python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 if [ $? -ne 0 ]; then
   echo "Failed to collect static files!"
   exit 1
