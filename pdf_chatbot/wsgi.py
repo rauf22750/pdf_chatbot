d14d79a760aa35920b46817eafd3642080bcc5f8
Abdul Rauf <rauf22750@gmail.com>
2025-01-28 19:06:00 +0500

    first commit

--- pdf_chatbot/wsgi.py
+++ pdf_chatbot/wsgi.py
@@ -1,16 +1,13 @@
+# pdf_chatbot/wsgi.py
+
 import os
 from django.core.wsgi import get_wsgi_application
 
-print("WSGI application starting...")
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pdf_chatbot.settings')
 
-try:
-    application = get_wsgi_application()
-    print("WSGI application loaded successfully.")
-except Exception as e:
-    print(f"WSGI error: {e}")
-
+application = get_wsgi_application()
 app = application
+
+
 
 
