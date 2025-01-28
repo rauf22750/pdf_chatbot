0aaf7edc52331155e45636d7caf95ebcc9cb1bde
Abdul Rauf <rauf22750@gmail.com>
2025-01-28 19:28:30 +0500

    first commit

--- pdf_chatbot/asgi.py
+++ pdf_chatbot/asgi.py
@@ -6,12 +6,16 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
-
 import os
-
+import logging
 from django.core.asgi import get_asgi_application
+
+logger = logging.getLogger(__name__)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pdf_chatbot.settings')
 
-application = get_asgi_application()
-app = application
+try:
+    application = get_asgi_application()
+except Exception as e:
+    logger.error(f"Error while getting ASGI application: {e}")
+    raise
--- pdf_chatbot/wsgi.py
+++ pdf_chatbot/wsgi.py
@@ -1,13 +1,17 @@
 import os
+import logging
 from django.core.wsgi import get_wsgi_application
-# from django.core.management import execute_from_command_line
+
+logger = logging.getLogger(__name__)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pdf_chatbot.settings')
 
-# Collect static files
-# execute_from_command_line(['manage.py', 'collectstatic', '--noinput'])
+try:
+    application = get_wsgi_application()
+except Exception as e:
+    logger.error(f"Error while getting WSGI application: {e}")
+    raise
 
-application = get_wsgi_application()
 app = application
 
 
