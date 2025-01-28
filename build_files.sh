9a363e72df2ba9ab54b0255a8726e8b7bd7ba624
Abdul Rauf <rauf22750@gmail.com>
2025-01-28 18:49:17 +0500

    first commit

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
-#!/bin/bash
+set -e  # Exit on error
+
 echo "Installing dependencies..."
 python3.9 -m pip install --upgrade pip
+if [ $? -ne 0 ]; then
+  echo "Failed to upgrade pip!"
+  exit 1
+fi
+
 python3.9 -m pip install -r requirements.txt
+if [ $? -ne 0 ]; then
+  echo "Failed to install dependencies from requirements.txt!"
+  exit 1
+fi
+
 echo "Collecting static files..."
 python3.9 manage.py collectstatic --noinput
+if [ $? -ne 0 ]; then
+  echo "Failed to collect static files!"
+  exit 1
+fi
+
 echo "Build completed."
