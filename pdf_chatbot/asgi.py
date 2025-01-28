4919d50eadf85f1be64e99fb8fb982aac2e5eb0b
Abdul Rauf <rauf22750@gmail.com>
2025-01-28 19:23:28 +0500

    first commit

--- pdf_chatbot/asgi.py
+++ pdf_chatbot/asgi.py
@@ -14,3 +14,4 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pdf_chatbot.settings')
 
 application = get_asgi_application()
+app = application
--- pdf_chatbot/wsgi.py
+++ pdf_chatbot/wsgi.py
@@ -1,11 +1,11 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from django.core.management import execute_from_command_line
+# from django.core.management import execute_from_command_line
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pdf_chatbot.settings')
 
 # Collect static files
-execute_from_command_line(['manage.py', 'collectstatic', '--noinput'])
+# execute_from_command_line(['manage.py', 'collectstatic', '--noinput'])
 
 application = get_wsgi_application()
 app = application
