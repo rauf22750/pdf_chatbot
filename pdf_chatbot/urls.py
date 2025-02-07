2bbb6d9c3c2c2f5fa7c778fc511ca8e9624e05ac
Abdul Rauf <rauf22750@gmail.com>
2025-02-07 20:46:17 +0500

    first commit

--- pdf_chatbot/urls.py
+++ pdf_chatbot/urls.py
@@ -32,5 +32,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_ROOT, document_root=settings.STATIC_ROOT)
+
 
 
